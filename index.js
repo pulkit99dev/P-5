1b33ec6d71a298f735682f9b3a2928a20bdaaeb8
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-23 16:46:50 +0530

    setting up views & layouts

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const port = 9000;
 const db = require('./config/mongoose')
+const ExpressLayouts = require('express-ejs-layouts');
+const assets = require('./assets')
 
 
 const app = express();
+
+//setting up views
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+//setting up express layouts
+app.use(ExpressLayouts());
+
+
 
 app.use(express.urlencoded());
 
