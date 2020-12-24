b7b878472a7cd4206d37d176fabaf410b859bb89
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-24 13:05:12 +0530

    saving user db

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -24,10 +24,14 @@ module.exports.create = function(req, res){
     }
     User.findOne({email : req.body.email}, function(err, user){
         if(err){console.log(`Error while creating user`); return;}
+        if(!user){
         User.create(req.body, function(err, user){
             if(err){console.log('Error while creating user');
-            return res.redirect('/user/signin')}
-
-        })
-    })
+            return res.redirect('/user/sign-in')};
+        
+        });
+    }else{
+            return res.redirect('back')
+        };
+        });
 }
